5c6ebdbaa25f728933937b62d34f2e39e4ca00ac
manuchap <manuchap@gmail.com>
2017-04-17 19:00:50 +0200

    init

---
+++ public/javascripts/application.js
@@ -0,0 +1,17 @@
+$("#searchterm").keyup(function (e) {
+  var q = $("#searchterm").val();
+  $.getJSON("http://en.wikipedia.org/w/api.php?callback=?",
+    {
+      srsearch: q,
+      action: "query",
+      list: "search",
+      format: "json"
+    },
+    function (data) {
+      $("#results").empty();
+      $("#results").append("<p>Results for <b>" + q + "</b></p>");
+      $.each(data.query.search, function (i, item) {
+        $("#results").append("<div><a href='http://en.wikipedia.org/wiki/" + encodeURIComponent(item.title) + "'>" + item.title + "</a><br>" + item.snippet + "<br><br></div>");
+      });
+    });
+});

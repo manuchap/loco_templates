e7ff3e8dbf1ee0be777b75fb08d72b9d8bf06889
manuchap <manuchap@gmail.com>
2017-04-22 02:07:26 +0200

    file update ok

--- public/javascripts/application.js
+++
@@ -1,8 +0,0 @@
-function updateMember() {
-  var nick = document.getElementById('nick').value;
-  var firs = document.getElementById('firs').value;
-  var last = document.getElementById('last').value;
-  var desc = document.getElementById('desc').value;
-  var yurl = "/saving?nick=" + nick + "&firs=" + firs + "&last=" + last + "&desc=" + desc;
-  window.location = yurl;
-}
